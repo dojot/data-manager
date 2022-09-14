8956d10486c11ad80b0d0f14080660826716b54a
Jefferson Coutinho <coutinho_santos@yahoo.com.br>
2022-09-14 16:59:17 -0300

    Revert "adjust codefactor"
    This reverts commit 74176f6b8b1314fb2bc772bd87e6dae8564cf01c.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import { logger } from '@dojot/dojot-module-logger';
 import express from 'express';
 import bodyParser from 'body-parser';
 import Export from './routers/export';
 import Import from './routers/import';
 import config from './config';
-
+import { logger } from '@dojot/dojot-module-logger';
 
 const app = express();
 app.use(bodyParser.json({ limit: config.request_body_limit }));
 app.use(Export);
 app.use(Import);
 app.listen(config.port, () => {
-  logger.setSid('data-manager');
+  logger.setSid('data-manager');  
   console.log(`server running on port ${config.port}`);
 });
